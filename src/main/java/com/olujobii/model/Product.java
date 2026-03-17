ec7b6e67669f1d42be0d942acf5c38ae8c66b632
Ifeouwa Olujobi <ifeoluwaolujobi@gmail.com>
2026-03-17 06:04:01 +0100

    Creating unique ID generator

--- src/main/java/com/olujobii/model/Product.java
+++ src/main/java/com/olujobii/model/Product.java
@@ -1,7 +1,19 @@
 package com.olujobii.model;
 
-public record Product(String id, String name, double price, int quantity, String category) {
+import com.olujobii.enums.ProductCategory;
 
+public record Product(String id, String name, double price, int quantity, ProductCategory category) {
+    private static int idGenerator = 1;
+
+    public Product(String id, String name, double price, int quantity, ProductCategory category){
+       this.id = id + idGenerator;
+       this.name = name;
+       this.price = price;
+       this.quantity = quantity;
+       this.category = category;
+
+       idGenerator++;
+    }
     public double getTotalValue(){
         return price * quantity;
     }
