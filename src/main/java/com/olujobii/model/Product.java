ea75ca8bda8b6f7085abd2b7e6f10a9c138554b6
Ifeouwa Olujobi <ifeoluwaolujobi@gmail.com>
2026-03-18 04:06:48 +0100

    Removed cconstructor in record class

--- src/main/java/com/olujobii/model/Product.java
+++ src/main/java/com/olujobii/model/Product.java
@@ -3,17 +3,7 @@ package com.olujobii.model;
 import com.olujobii.enums.ProductCategory;
 
 public record Product(String id, String name, double price, int quantity, ProductCategory category) {
-    private static int idGenerator = 1;
 
-    public Product(String id, String name, double price, int quantity, ProductCategory category){
-       this.id = id + idGenerator;
-       this.name = name;
-       this.price = price;
-       this.quantity = quantity;
-       this.category = category;
-
-       idGenerator++;
-    }
     public double getTotalValue(){
         return price * quantity;
     }
